82771da8990e04edda951fb11df9da13140fbc91
Artem Vovsia <artemvovsia@fb.com>
2020-03-04 10:52:46 +0000

    Dotfiles 2020. Start

---
+++ install.sh
@@ -0,0 +1,6 @@
+# Brew 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# ZSH
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
